cff3ca5c17e387b7ca2859136237b7a4af4a8ab2
vticr7 <vti9227@gmail.com>
2023-03-22 22:06:44 +0530

    another firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 import { getStorage, ref } from "firebase/storage";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,3 +21,4 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
 export const storage = getStorage();
+export const db = getFirestore()
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,68 +1,77 @@
 import React, { useState } from "react";
 import Add from "../image/upload.png"
-
-import {createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
-import {auth,storage} from "../firebase"
-import {  ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
-
+// vaibhav vaibhav16
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth, db, storage } from "../firebase"
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { doc, setDoc } from "firebase/firestore";
 
 
 const Register = () => {
-    const[err,setErr]=useState(false)
+    const [err, setErr] = useState(false)
 
-    const handleSubmit = async(e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault()
-        const displayName=e.target[0].value;
-        const email=e.target[1].value;
-        const password =e.target[2].value;
-        const file=e.target[3].files[0];
+        const displayName = e.target[0].value;
+        const email = e.target[1].value;
+        const password = e.target[2].value;
+        const file = e.target[3].files[0];
 
         // const auth = getAuth();
-        try{      // const auth = getAuth();
-        const res=createUserWithEmailAndPassword(auth, email, password)
+        try {      // const auth = getAuth();
+            const res = createUserWithEmailAndPassword(auth, email, password)
+
+
+            const storageRef = ref(storage, displayName);
+
+            const uploadTask = uploadBytesResumable(storageRef, file);
+
+            // Register three observers:
+            // 1. 'state_changed' observer, called any time the state changes
+            // 2. Error observer, called on failure
+            // 3. Completion observer, called on successful completion
+            uploadTask.on(
+                (error) => {
+                    setErr(true);
+                    // Handle unsuccessful uploads
+                },
+                () => {
+                    // Handle successful uploads on complete
+                    // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+                    getDownloadURL(storageRef).then(async (downloadURL) => {
+                        
+                        await updateProfile(res.user, {
+                            displayName,
+                            photoURL: downloadURL,
+                          });
+                          await setDoc(doc(db, "users", res.user.uid), {
+                            uid: res.user.uid,
+                            displayName,
+                            email,
+                            photoURL: downloadURL,
+                          });
+                        await setDoc(doc(db, "userChats",(await res).user.uid),{})
+                        
+                    });
+                }
+            );
             
- 
-        const storageRef = ref(storage, displayName);
-
-        const uploadTask = uploadBytesResumable(storageRef, file);
-
-// Register three observers:
-// 1. 'state_changed' observer, called any time the state changes
-// 2. Error observer, called on failure
-// 3. Completion observer, called on successful completion
-uploadTask.on( 
-  (error) => {
-    setErr(true); 
-    // Handle unsuccessful uploads
-  }, 
-  () => {
-    // Handle successful uploads on complete
-    // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-    getDownloadURL(uploadTask.snapshot.ref).then(async(downloadURL) => {
-     await updateProfile((await res).user,{
-        displayName,
-        photoURL:downloadURL,
-
-     })
-    });
-  }
-);
-        }catch(err){
+        } catch (err) {
             setErr(true);
         }
 
 
-//   .then((userCredential) => {
-//     // Signed in 
-//     const user = userCredential.user;
-//     // console.log(user)
-//     // ...
-//   })
-//   .catch((error) => {
-//     const errorCode = error.code;
-//     const errorMessage = error.message;
-//     // ..
-//   });
+        //   .then((userCredential) => {
+        //     // Signed in 
+        //     const user = userCredential.user;
+        //     // console.log(user)
+        //     // ...
+        //   })
+        //   .catch((error) => {
+        //     const errorCode = error.code;
+        //     const errorMessage = error.message;
+        //     // .. 
+        //   });
 
     }
     return (
